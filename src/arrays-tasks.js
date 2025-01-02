005f3597fcfc15a4c1a2ca6be1a10adc22c7a1bb
Michael <fatlycka@gmail.com>
2025-01-02 17:10:17 +0300

    feat: did 3 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -456,8 +456,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arr = [];
+  numbers.map((el, i) => {
+    return el % (+!![] + +!!{}) !== +![] ? arr.push(i) : [] + [];
+  });
+  return arr;
 }
 
 /**
@@ -470,8 +474,13 @@ function getIndicesOfOddNumbers(/* numbers */) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map(
+    (el) =>
+      `#${'0'.repeat(6 - el.toString(16).length)}${el
+        .toString(16)
+        .toUpperCase()}`
+  );
 }
 
 /**
@@ -488,8 +497,8 @@ function getHexRGBValues(/* arr */) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr.sort((a, b) => b - a).slice(0, n);
 }
 
 /**

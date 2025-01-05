aa04732b60c1b218196df7125a1d6bd931240017
Michael <fatlycka@gmail.com>
2025-01-05 03:41:00 +0300

    feat: complete arrays-tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -270,8 +270,13 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n === 1) {
+    return Array.from({ length: size }).fill(0);
+  }
+  return Array.from({ length: size }).fill(
+    createNDimensionalArray(n - 1, size)
+  );
 }
 
 /**
@@ -528,8 +533,20 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(num) {
+  let count = 0;
+  return Math.max(
+    ...[
+      ...num.map((el, i) => {
+        if (!num[i - 1] || el < num[i - 1]) {
+          count = 1;
+          return count;
+        }
+        count += 1;
+        return count;
+      }),
+    ]
+  );
 }
 
 /**
